33c4fe54845f5680577f0d64b67f939cb18390bd
lurigore <153800413+lurigore@users.noreply.github.com>
2023-12-15 19:48:35 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -10,15 +10,18 @@ const observer = new IntersectionObserver((entries) => {
 });
 
 const hiddenElements = document.querySelectorAll(".pages");
-hiddenElements.forEach((el) => observer.observe(el))
-
+hiddenElements.forEach((el) => {
+    observer.observe(el);
+})
+let path = "";
 const photosContainer = document.querySelector(".photos-grid");
+
+const displayImage = document.querySelector(".image-display");
 function getIndex()
 {
         fetch('./data/json/photos.json')
     .then(response => response.json())
     .then(data => {
-        
         for(let i = 0; i <= (data.length - 1); i++)
         {
             const image = document.createElement("img");
@@ -26,10 +29,21 @@ function getIndex()
             div.classList.add("photo-obj")
             image.src = `./data/fotos/${data[i].tipo}/${data[i].source}`;
             image.classList.add("created-image");
+            div.classList.add('hide');
             div.setAttribute("id", data[i].index);
             div.appendChild(image);
             photosContainer.appendChild(div);
-            
+            image.addEventListener('click', function(){
+                console.log(image.src);
+                displayImage.style = "filter: blur(100px)";
+                setTimeout(function(){
+                    displayImage.src = image.src;
+                },300)
+                setTimeout(function(){
+                    displayImage.style = "filter: blur(0px)";
+                },300)
+            });
+
         }
     })
     .catch(error => {
@@ -37,22 +51,34 @@ function getIndex()
     });
 }
 getIndex();
-let typeIndex = 1;
+let typeIndex = 0;
 function selectType(index){
     typeIndex = index;
-    console.log(typeIndex);
 
     const allPhotos = document.querySelectorAll(".photo-obj");
     for(let i = 0; i < allPhotos.length; i++)
     {
-        console.log(allPhotos[i].id);
         if(parseInt(allPhotos[i].id) !== typeIndex)
         {
             allPhotos[i].classList.add("hide");
-            console.log(allPhotos[i].src);
         }
         else{
             allPhotos[i].classList.remove("hide");
         }
     }
 }
+
+const grid = document.querySelector(".grid-photos");
+const photosGrid = document.querySelector(".photos-grid");
+ShowGrid();
+function ShowGrid()
+{
+    const tipos = document.querySelectorAll(".tipoDeFoto");
+    tipos.forEach(foto =>{
+        foto.addEventListener('click', function(){
+            grid.style = "width:40rem;";
+            setTimeout(function(){
+                photosGrid.style = "display:grid;";
+            }, 390);
+    })
+})}
